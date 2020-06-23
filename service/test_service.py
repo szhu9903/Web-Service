a92b63b30e3539633d96a6deaa0794a295724455
15994092160 <1781082307@qq.com>
2020-06-23 20:19:11 +0800

    web-service

--- service/test_service.py
+++ service/test_service.py
@@ -2,14 +2,14 @@
 
 from spyne import rpc,Application,ServiceBase
 from spyne import String,Unicode,Array,ComplexModel,Integer,table,Iterable
-
+import json
 
 class TestService(ServiceBase):
-
     @rpc(String,_returns=Iterable(String))
     def get_test(self,test_name):
         try:
-            yield 'name'+test_name
+            print(test_name)
+            yield json.dumps({'name':test_name})
         except:
             yield 'error'
 
--- soap.py
+++ soap.py
@@ -1,5 +1,31 @@
 # -*- coding: utf-8 -*-
 
-from spyne.application import Application
+from wsgiref.simple_server import make_server
+
+from spyne import Application
+# soap协议
+from spyne.protocol.soap import Soap11
+# 测试
+from spyne.server.wsgi import WsgiApplication
+
+
+applicaction = Application([],
+                           'mytest',
+                           in_protocol = Soap11(validator = 'lxml'),
+                           out_protocol = Soap11())
+wsgi_application = WsgiApplication(applicaction)
+
+if __name__ == '__main__':
+    import logging
+
+    host = '0.0.0.0'
+    port = 8001
+
+    logging.info('listening to http://127.0.0.1:8001')
+    logging.info('wsdl is at: http://127.0.0.1:8001/?wsdl')
+
+    server = make_server(host,port,wsgi_application)
+    server.serve_forever()
+
 
 
